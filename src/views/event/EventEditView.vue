e9af95eca6f0be3154306ba1582fcacbcb5664de
Ratchaporn <hrkfan321@gmail.com>
2023-08-13 15:31:56 +0700

    Lab 04 (1.7)

--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -2,14 +2,31 @@
 import type { Ref } from 'vue'
 import type { EventItem } from '@/type'
 import type { PropType } from 'vue'
+import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
-defineProps({
+const props = defineProps({
   event: {
     type: Object as PropType<EventItem>,
     require: true
   }
 })
+const router = useRouter()
+const store = useMessageStore()
+function edit() {
+  store.updateMessage('You have successfully edit. ' + props.event?.title)
+  setTimeout(() => {
+    store.resetMessage()
+  }, 3000)
+  router.push({
+    name: 'event-detail',
+    params: {
+      id: props.event?.id
+    }
+  })
+}
 </script>
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit here</button>
 </template>
